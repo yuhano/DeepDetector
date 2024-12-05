e80bb648900a8999b93845e9433adddf1c6a5329
yuhano <30584359+yuhano@users.noreply.github.com>
2024-12-05 11:54:18 +0900

    dashboard css 수정

--- ipfire/api/__init__.py
+++ ipfire/api/__init__.py
@@ -4,8 +4,6 @@ from flask import Blueprint
 api_bp = Blueprint('api', __name__)
 
 # 엔드포인트 등록
-from .auth import auth_blueprint
 from .logs import logs_blueprint
 
-api_bp.register_blueprint(auth_blueprint, url_prefix='/auth')
 api_bp.register_blueprint(logs_blueprint, url_prefix='/logs')
