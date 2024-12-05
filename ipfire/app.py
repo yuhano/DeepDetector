f24058d31b05388ac17e82dd6ac1500fa4a4bc81
yuhano <30584359+yuhano@users.noreply.github.com>
2024-12-05 11:47:57 +0900

    ipfire 인증 로직 삭제

--- ipfire/app.py
+++ ipfire/app.py
@@ -1,21 +1,15 @@
 from flask import Flask, render_template
-from api.auth import auth_blueprint
 from api.logs import logs_blueprint
 
 def create_app():
     app = Flask(__name__)
 
-    # HTML 페이지 라우트
-    @app.route('/')
-    def victim():
-        return render_template('victim.html')
 
-    @app.route('/dashboard')
+    @app.route('/')
     def dashboard():
         return render_template('dashboard.html')
 
     # API 블루프린트 등록
-    app.register_blueprint(auth_blueprint, url_prefix='/api/auth')
     app.register_blueprint(logs_blueprint, url_prefix='/api/logs')
 
     return app
@@ -23,4 +17,4 @@ def create_app():
 if __name__ == '__main__':
     print("start")
     app = create_app()
-    app.run(debug=True, host='0.0.0.0', port=3000)
+    app.run(debug=True, host='0.0.0.0', port=4000)
